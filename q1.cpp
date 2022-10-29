dae951f9ab88c98225269c8b929037f266cb558a
Tem <dxpetrenko840@zonemail.clpccd.edu>
2022-10-28 17:31:07 -0700

    fixign tests 3

--- q1.cpp
+++ q1.cpp
@@ -18,14 +18,14 @@ int main()
 	cout << " After Swap" << setw(5) << num1 << setw(5) << num2 << setw(5) << endl;
 
 	getinput(fnum1, fnum2);
-	cout << setprecision(2) << 
+	cout << " Before Swap" << setprecision(2) << " " <<
 	fnum1 << setprecision(2) << 
 	
 	fnum2 << setprecision(2) << endl;
 	swapTwoValues(fnum1, fnum2);
-	cout << setprecision(2) << 
-	fnum1 << setprecision(2) << 
-	
+	cout << " After Swap" 
+	<< setprecision(2) << 
+	fnum1 << setprecision(2) << " " <<
 	fnum2 << setprecision(2) << endl;
 }
 
